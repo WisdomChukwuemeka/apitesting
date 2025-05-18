0f6e6e41fded2bcb119e6133b1335111b376dfa5
wisdom chukwuemeka <wisdomchukwuemeka97@gmail.com>
2025-05-18 08:01:52 +0100

    code modification

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import { Home } from "../components/home"
 export const App = () => {
   return(
     <>
-    <div class="flex flex-col min-h-screen">
+    <div class="flex flex-col min-h-screen relative">
       <Layout />
     <Home />
 
-    <div class="bg-black text-white h-fit w-full text-center fixed bottom-0 p-2 md:text-2xl lg:text-3xl">
+    <div class="bg-black text-white h-fit w-full text-center absolute bottom-0 p-2 md:text-2xl lg:text-3xl">
         <Footer />
     </div>
     </div>
